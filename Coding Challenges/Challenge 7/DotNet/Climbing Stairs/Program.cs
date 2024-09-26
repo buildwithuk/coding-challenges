5622956d5e3e4626a11e81c5e45f25b7e2a164fc
Umair Khan <umairk83@live.com>
2024-09-26 23:44:36 +0500

    Climbing stairs

---
+++ Coding Challenges/Challenge 7/DotNet/Climbing Stairs/Program.cs
@@ -0,0 +1,35 @@
+﻿
+
+using System.Collections;
+
+static int returnStairCount(int noOfStairs) {
+
+    int[] noOfCount = new int[noOfStairs+1];
+
+    // No of count will always be one
+    if ( noOfStairs == 1) {
+        return 1;
+    }
+
+    noOfCount[0] = 1;
+    noOfCount[1] = 1;
+
+    for (int i = 2; i < noOfCount.Length; i++) {
+
+        noOfCount[i] = noOfCount[i-2] + noOfCount[i-1];
+
+
+    }
+    return noOfCount[noOfStairs];
+
+
+}
+
+
+var r = returnStairCount(4);
+
+
+Console.WriteLine("No of count: " + r);
+
+
+
