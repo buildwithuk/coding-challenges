f50246b2b3484fd2ba6008e29a32fa9b61dc2c49
Uk <umairk83@live.com>
2025-10-20 02:50:33 +0500

    Added code for linked list

---
+++ leet-code-in-java-dotnet/Section 3 - Linked Lists/src/Start.java
@@ -0,0 +1,134 @@
+
+import implement.linkedlist.LinkedList;
+import implement.linkedlist.Node;
+
+public class Start {
+
+	public static void main(String[] args) {
+
+		System.out.println("----------- Printing List -----------");
+		Node<String> headNode = new Node<>("Umair");
+		LinkedList<String> studentNamesList = new LinkedList<String>(headNode);
+
+		System.out.println("Length of the list: " + studentNamesList.getLength());
+		System.out.println("Head node: " + studentNamesList.getHead());
+		System.out.println("Tail node: " + studentNamesList.getTail());
+
+		studentNamesList.printList();
+
+		System.out.println("----------- Appending List -----------");
+		LinkedList<String> emptyList = new LinkedList<>();
+
+		// Create a node to append;
+		emptyList.append("First student");
+		emptyList.append("Second student");
+		emptyList.append("Third student");
+
+		emptyList.printList();
+
+		System.out.println("----------- Popping List -----------");
+		// Pop the first element
+		System.out.println(emptyList.pop());
+		System.out.println(emptyList.pop());
+
+		System.out.println("----------- Pop List -----------");
+		LinkedList<Integer> marksList = new LinkedList<>();
+
+		// When the lsit if empty
+		System.out.println(marksList.pop());
+
+		// Add only one node
+		marksList.append(1);
+		System.out.println(marksList.pop());
+
+		System.out.println("----------- Prepending List -----------");
+
+		// Tests for the prepend
+		LinkedList<String> billList = new LinkedList<String>();
+
+		billList.prepend("First bill");
+		billList.prepend("Second List bill");
+		billList.prepend("Third List bill");
+		billList.prepend("Fourth List bill");
+
+		billList.printList();
+
+		System.out.println("----------- Pop First -----------");
+
+		LinkedList<Float> currencyLinkedList = new LinkedList<>();
+		currencyLinkedList.append(1F);
+		currencyLinkedList.append(2F);
+		currencyLinkedList.printList();
+		currencyLinkedList.popFirst();
+
+		currencyLinkedList.printList();
+
+		System.out.println("----------- Get and set value Method -----------");
+
+		LinkedList<Double> savingsList = new LinkedList<>();
+		try {
+			savingsList.get(2);
+		} catch (IndexOutOfBoundsException exception) {
+			System.out.println(exception.getMessage());
+		}
+
+		savingsList.append(1.3);
+		savingsList.append(Double.valueOf(3));
+		savingsList.append(Double.valueOf(8));
+
+		savingsList.printList();
+
+		System.out.println(savingsList.get(0));
+		System.out.println(savingsList.get(1));
+		System.out.println(savingsList.get(2));
+
+		// Update the value of the list
+		System.out.println(savingsList.setValue(0, 0.1));
+		System.out.println(savingsList.setValue(1, 33.3));
+		System.out.println("Value of the node at the index is " + savingsList.get(0));
+		System.out.println("Value of the node at the index is " + savingsList.get(1));
+		System.out.println("\n\n ---------------------------- \n\n");
+	
+		savingsList.printList();
+
+		System.out.println("\n\n----------- Insert Method -----------");
+		System.out.println("Result of insertion: " + savingsList.insert(1, 0.5));		
+		savingsList.printList();
+		
+		System.out.println("Result of insertion: " + savingsList.insert(6, 0.5));
+		savingsList.printList();
+		
+		
+		System.out.println("\n\n----------- Remove Method -----------");
+		savingsList.remove(1);
+		
+		savingsList.printList();
+		
+		savingsList.remove(0);
+		savingsList.printList();
+		
+		
+		savingsList.remove(savingsList.getLength() - 1);
+		
+		savingsList.printList();
+		
+		System.out.println("\n\n----------- Reverse List Method -----------");
+		
+		
+		LinkedList<String> citiesList = new LinkedList<>();
+		
+		citiesList.append("Karachi");
+		citiesList.append("Munich");
+		
+		System.out.println("** BEFORE **");
+		citiesList.printList();
+		System.out.println("** AFTER **");
+		
+		citiesList.reverseList();
+		
+
+		citiesList.printList();
+		
+	}
+
+}
---
+++ leet-code-in-java-dotnet/Section 3 - Linked Lists/src/implement/linkedlist/LinkedList.java
@@ -0,0 +1,327 @@
+package implement.linkedlist;
+
+public class LinkedList<T> {
+
+	private Node<T> head;
+	private Node<T> tail;
+	private int length;
+
+	public boolean prepend(T value) {
+
+		// Node to prepend
+		Node<T> nodeToPrepend = new Node<T>(value);
+
+		if (this.length == 0) {
+			this.head = nodeToPrepend;
+			this.tail = nodeToPrepend;
+
+		} else {
+
+			Node<T> tempNode = this.head;
+
+			this.head = nodeToPrepend;
+			nodeToPrepend.setNextNode(tempNode);
+		}
+
+		System.out.println("Length of the list after prepending: " + ++this.length);
+		return true;
+	}
+
+	public Node<T> popFirst() {
+
+		if (this.length == 0) {
+			return null;
+		} else {
+			Node<T> nodeToPop = null;
+
+			// N1 -> N2 -> N3 -> N4
+			nodeToPop = this.head;
+			this.head = nodeToPop.getNextNode();
+
+			// The list is empty after removing the node
+			if (this.head.getNextNode() == null) {
+				this.tail = this.head;
+			}
+
+			nodeToPop.setNextNode(null);
+			this.length--;
+
+			return nodeToPop;
+		}
+	}
+
+	public Node<T> pop() {
+
+		// Iterate over the list
+
+		Node<T> currentNode = this.head;
+		Node<T> tempNode = this.head;
+		Node<T> previousNode = null;
+
+		while (currentNode != null) {
+
+			// This means that this is the tail node
+			// N1 -> N2 -> N3 -> N4 -> N5
+			if (currentNode.getNextNode() == null) { // We have reached the tail
+
+				// Current node is the tail
+				// Previous node is one node behind current node
+				tempNode = currentNode;
+				// Update the previous node
+				if (previousNode != null) {
+					previousNode.setNextNode(null);
+					this.tail = previousNode;
+				} else {
+					this.tail = null;
+					this.head = null;
+				}
+
+				this.length--;
+				currentNode = null; // This will break the loop
+
+			} else {
+
+				tempNode = currentNode;
+				currentNode = currentNode.getNextNode();
+				previousNode = tempNode;
+
+			}
+		}
+
+		System.out.println("Length of the list after pop: " + this.length);
+		return tempNode;
+	}
+
+	public void reverseList() {
+
+		if (this.length == 0) {
+			System.out.println("List is empty!");
+			return;
+		} else if (this.length == 1) {
+			System.out.println("Nothing to do");
+			return;
+		} else {
+
+			Node<T> tempNode = this.head;
+			
+			this.head = this.tail;
+			this.tail = tempNode;
+			
+			Node<T> afterNode = tempNode.getNextNode();
+			
+			Node<T> beforeNode = null;
+			
+			
+			// N1 -> N2 -> N3 -> N4
+			
+			for (int a = 0; a < length; a++) {
+				
+				afterNode = tempNode.getNextNode();
+				tempNode.setNextNode(beforeNode);
+				beforeNode = tempNode;
+				tempNode = afterNode;
+				
+			}
+			
+		}
+	}
+
+	public LinkedList(Node<T> node) {
+
+		this.head = node;
+		this.setTail(node);
+		length = 1;
+	}
+
+	// Empty Constructor
+	public LinkedList() {
+		this.length = 0;
+	}
+
+	public int getLength() {
+		return length;
+	}
+
+	public Node<T> getHead() {
+		return head;
+	}
+
+	public void setHead(Node<T> head) {
+		this.head = head;
+	}
+
+	public Node<T> getTail() {
+		return tail;
+	}
+
+	public void setTail(Node<T> tail) {
+		this.tail = tail;
+	}
+
+	public void printList() {
+
+		if (this.length == 0) {
+			return;
+		}
+
+		Node<T> node = this.head;
+
+		System.out.println("Length of the list: " + length);
+
+		boolean isHead = true;
+
+		while (node != null) {
+
+			System.out.println("--- " + (isHead ? "(Head)" : "") + node.toString() + " ---");
+			node = node.getNextNode();
+			isHead = false;
+		}
+	}
+
+	public void append(T value) {
+
+		// Node to append
+		Node<T> nodeToAppend = new Node<T>(value);
+
+		// N1 -> N2-> N3 -> N4 -> N5
+		// N1 -> N2-> N3 -> N4 -> N5 -> NT
+
+		if (this.head == null) {
+			this.head = nodeToAppend;
+			this.tail = nodeToAppend;
+			this.length = 1;
+		} else {
+
+			// Temp node
+			Node<T> tempNode = this.tail;
+			this.tail = nodeToAppend;
+			tempNode.setNextNode(nodeToAppend);
+
+			this.length++;
+		}
+
+		System.out.println("Length of the list is " + this.length + " after appending the value " + value.toString());
+	}
+
+	public boolean setValue(int index, T value) {
+
+		try {
+
+			// Find the node
+			Node<T> node = get(index);
+			node.setValue(value);
+			return true;
+		} catch (IndexOutOfBoundsException ex) {
+			System.out.println(ex.getMessage());
+			return false;
+		}
+	}
+
+	public Node<T> get(int index) {
+
+		// Initialize the node that needs to be returned
+		Node<T> node = null;
+
+		// Because the list is empty
+		if (this.length == 0) {
+			throw new IndexOutOfBoundsException("Linked List is empty");
+		}
+
+		node = this.head;
+
+		if (index == 0) {
+			return node;
+		}
+
+		int counter = 1;
+
+		// N1 -> N2 -> N3 -> N4 -> N5
+// index  0     1     2
+
+		while (counter <= index) {
+
+			node = node.getNextNode();
+
+			if (node.getNextNode() == null && counter < index) {
+				throw new IndexOutOfBoundsException(
+						String.format("Value of counter: %d and the index: %d", counter, index));
+			}
+			counter++;
+		}
+		return node;
+
+	}
+
+	public Node<T> remove(int index) {
+
+		try {
+
+			Node<T> n;
+
+			if (index == 0) {
+				n = popFirst();
+			} else if (index == length - 1) {
+				n = pop();
+			} else {
+
+				// N1 -> N2 -> N3 -> N4 -> N5
+				// 0 1 2 3 4
+				//
+				n = get(index); // Imagine index = 2
+				Node<T> nodeAtPreviousIndex = get(index - 1); // Imagine index = 1
+				Node<T> tempNode = n.getNextNode();
+				nodeAtPreviousIndex.setNextNode(tempNode);
+				n.setNextNode(null);
+				length--;
+			}
+
+			return n;
+
+		} catch (IndexOutOfBoundsException ex) {
+			return null;
+		}
+
+	}
+
+	public boolean insert(int index, T value) {
+
+		try {
+
+			// We are adding to the head
+			if (index == 0) {
+				prepend(value);
+			} else if (index == length - 1) { // Adding at the tail
+				append(value);
+			} else {
+				// N0 -> N1 -> N2 -> N3 -> N4 (Insert into N3.4)
+				// N1.5
+				// Get the node at the index
+				Node<T> nodeAtIndex = get(index - 1);
+
+				System.out.println("Fetched the node at the index: " + index + " " + nodeAtIndex.getValue());
+
+				// Create the node that needs to be inserted
+				Node<T> nodeToInsert = new Node<>(value);
+
+				// Save the next node of the index node
+				Node<T> tempNode = nodeAtIndex.getNextNode();
+
+				// Set the next of the node to isnert to the next of the node of the index
+				nodeToInsert.setNextNode(tempNode);
+
+				// Set the next node of the current node to the new node to be inserted
+				nodeAtIndex.setNextNode(nodeToInsert);
+
+			}
+
+			// Incremennt the length of the list
+			this.length++;
+
+			return true;
+
+		} catch (IndexOutOfBoundsException ex) {
+			return false;
+		}
+	}
+
+}
